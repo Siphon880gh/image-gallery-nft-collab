de2af449d431fca6642beef31162f81392eadfb5
Siphon880gh <weng.f.fung@gmail.com>
2024-11-18 03:57:32 -0800

    Making compatible with a Hostinger VPS - More

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config({ path: '../.env' }); // Load environment variables from .env file
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reprints', {
   useNewUrlParser: true,
